0acb6770a72516fe1d0b0bfd6b0b1ace1f0a71d4
Alexander Forbes-Reed <git@alx.red>
2020-02-17 19:47:55 +0000

    Started working on the environment viewer

---
+++ Beak.App/Models/ProjectEnvironment.cs
@@ -0,0 +1,22 @@
+using System.Collections.Generic;
+using Beak.Common.Inpc;
+
+namespace Beak.App.Models
+{
+	public class ProjectEnvironment : BaseViewModel
+	{
+		public string Name
+		{
+			get { return _name; }
+			set { SetField(ref _name, value); }
+		}
+		private string _name = string.Empty;
+
+		public Dictionary<string, string> Mapping
+		{
+			get { return _mapping; }
+			set { SetField(ref _mapping, value); }
+		}
+		private Dictionary<string, string> _mapping = new Dictionary<string, string>();
+	}
+}
---
+++ Beak.App/Windows/Project/EnvironmentViewer.xaml.cs
@@ -0,0 +1,41 @@
+using System.Collections.Generic;
+using System.Windows.Controls;
+using Beak.App.Models;
+
+namespace Beak.App.Windows.Project
+{
+	/// <summary>
+	/// Interaction logic for EnvironmentViewer.xaml
+	/// </summary>
+	public partial class EnvironmentViewer : UserControl
+	{
+		public static ProjectEnvironment[] DesignDataContext = new ProjectEnvironment[]
+		{
+			new ProjectEnvironment
+			{
+				Name = "Environment",
+				Mapping = new Dictionary<string, string>
+				{
+					{ "Production", "" },
+					{ "Development", "" },
+				},
+			},
+			new ProjectEnvironment
+			{
+				Name = "Engineer",
+				Mapping = new Dictionary<string, string>
+				{
+					{ "Alex", "" },
+					{ "George", "" },
+				},
+			}
+		};
+
+		public EnvironmentViewer()
+		{
+			InitializeComponent();
+
+			DataContext = EnvironmentViewer.DesignDataContext;
+		}
+	}
+}
