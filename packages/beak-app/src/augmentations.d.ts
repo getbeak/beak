3a24793c527100f5d1f16b3b5df1f1a6f55fe48d
Alexander Forbes-Reed <git@alx.red>
2020-07-17 09:56:20 +0100

    More augmentations

--- packages/beak-app/src/augmentations.d.ts
+++ packages/beak-app/src/augmentations.d.ts
@@ -1,14 +1,22 @@
 import * as Electron from 'electron';
 import * as FsExtra from 'fs-extra';
 import * as Path from 'path';
+import { Store } from 'react-redux';
 
-// Don't like this, but have to as eslint doesn't understand interfaces, f
-/* eslint-disable no-undef */
+import { ApplicationState } from './store';
 
 declare global {
 	interface Window {
+		/* eslint-disable no-undef */
 		require(moduleSpecifier: 'electron'): typeof Electron;
 		require(moduleSpecifier: 'fs-extra'): typeof FsExtra;
 		require(moduleSpecifier: 'path'): typeof Path;
+		/* eslint-enable no-undef */
+
+		store: Store<ApplicationState>;
 	}
+}
+
+declare module 'react-redux' {
+	interface DefaultRootState extends ApplicationState {}
 }
