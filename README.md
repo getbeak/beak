687bd48311f4132e34cd738914002776db281226
Alexander Forbes-Reed <git@alx.red>
2020-07-19 20:37:16 +0300

    wee styling of the organiser (looks shit tho)

--- README.md
+++ README.md
@@ -2,8 +2,20 @@
 
 Snazzy cross-platform API development and research tool. Firstly in C#/XAML, now in Typescript/Electron. Vote opens soon for next rewrite.
 
+## Getting started
+
+1. Clone the git repo
+1. Run yarn
+1. Run yarn start
+1. ???
+1. profit
+
 ## Common issues
 
 ### A new `BrowserWindow` is instantiated but no window is created?
 
 If you've been fucking with `chrome-sandbox`, trying to get it working through WSL, it'll end up totally fucked. I've added a quick workaround for this, just run `yarn unfuck` in the root of the repo and it'll clean up everything for you.
+
+### The TS language server fails to detect electron being installed in the `beak-electron` package
+
+idk...
--- packages/beak-app/src/features/explorer-pane/components/organisms/Organiser.tsx
+++ packages/beak-app/src/features/explorer-pane/components/organisms/Organiser.tsx
@@ -3,16 +3,41 @@ import styled from 'styled-components';
 
 const Organiser: React.FunctionComponent = () => (
 	<Container>
-		<button>{'+'}</button>
-		<button>{'f'}</button>
+		<Button>{'+'}</Button>
+		<Button>{'⊞'}</Button>
 		{' | '}
-		<button>{'s'}</button>
-		<input />
+		<Button>{'⎈'}</Button>
+		<SearchBox placeholder={'search...'} />
 	</Container>
 );
 
 const Container = styled.div`
+	display: flex;
+	padding: 4px 6px;
+	padding-left: 2px;
 	border-top: 1px solid ${props => props.theme.ui.backgroundBorderSeparator};
+`;
+
+const Button = styled.button`
+	background: transparent;
+	border: none;
+	color: ${props => props.theme.ui.textOnSurfaceBackground};
+`;
+
+const SearchBox = styled.input`
+	flex-grow: 2;
+	background: transparent;
+	border: 1px solid ${props => props.theme.ui.backgroundBorderSeparator};
+	border-radius: 2px;
+
+	min-width: 70px;
+	font-size: 12px;
+	color: ${props => props.theme.ui.textOnSurfaceBackground};
+
+	&:focus {
+		outline: none;
+		border-color: ${props => props.theme.ui.primaryFill};
+	}
 `;
 
 export default Organiser;
