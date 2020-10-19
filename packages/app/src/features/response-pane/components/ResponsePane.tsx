798e62ebc278996107621ae29fd28af9e45984a9
Alexander Forbes-Reed <git@alex.forbes.red>
2020-10-19 21:39:02 +0100

    forgot unstaged changes

--- packages/app/src/features/response-pane/components/ResponsePane.tsx
+++ packages/app/src/features/response-pane/components/ResponsePane.tsx
@@ -1,16 +1,15 @@
-import { statusToColour } from '@beak/app/src/design-system/helpers';
 import { RequestNode } from '@beak/common/src/beak-project/types';
-import { constructUri } from '@beak/common/src/beak-project/url';
-import { getReasonPhrase } from 'http-status-codes';
-import React from 'react';
+import React, { useState } from 'react';
 import { useSelector } from 'react-redux';
 import styled from 'styled-components';
 
+import Header from './molecules/Header';
 import InspectorTabs from './organisms/InspectorTabs';
 
 const ResponsePane: React.FunctionComponent = () => {
 	const flight = useSelector(s => s.global.flight);
 	const { tree, selectedRequest } = useSelector(s => s.global.project);
+	const [selectedFlightIndex, setSelectedFlightIndex] = useState(0);
 	const selectedNode = tree![selectedRequest || 'non_existent'];
 
 	// TODO(afr): Maybe some sort of purgatory state here
@@ -21,41 +20,24 @@ const ResponsePane: React.FunctionComponent = () => {
 		throw new Error('fucked state?!');
 
 	const typedSelectedNode = selectedNode as RequestNode;
-	const flightHistory = flight.flightHistory[typedSelectedNode.id]?.[0];
+	const flightHistory = flight.flightHistory[typedSelectedNode.id];
+	const selectedFlightHistory = flightHistory[selectedFlightIndex];
 
 	// TODO(afr): Maybe some sort of purgatory state here
 	if (!flightHistory)
 		return <Container />;
 
-	const response = flightHistory.response;
-
 	return (
 		<Container>
-			<UrlHeaderWrapper>
-				<Section>
-					<strong>{flightHistory.request.uri.verb.toUpperCase()}</strong>
-				</Section>
-				<UrlSection>
-					{constructUri(flightHistory.request)}
-				</UrlSection>
-				<StatusSection $status={response.status}>
-					<strong>{response.status}</strong>
-					{' '}
-					{safeGetReasonPhrase(response.status)}
-				</StatusSection>
-			</UrlHeaderWrapper>
-			<InspectorTabs flight={flightHistory!} />
+			<Header
+				flightHistory={flightHistory}
+				selectedFlightIndex={selectedFlightIndex}
+				updateSelectedFlight={setSelectedFlightIndex}
+			/>
+			<InspectorTabs flight={selectedFlightHistory} />
 		</Container>
 	);
 };
-
-function safeGetReasonPhrase(status: number) {
-	try {
-		return getReasonPhrase(status);
-	} catch {
-		return '';
-	}
-}
 
 const Container = styled.div`
 	display: flex;
@@ -63,37 +45,6 @@ const Container = styled.div`
 	background-color: ${props => props.theme.ui.surface};
 	height: 100%;
 	width: 100%;
-`;
-
-const UrlHeaderWrapper = styled.div`
-	display: flex;
-	margin: 25px auto;
-
-	font-size: 15px;
-`;
-
-const Section = styled.div`
-	background-color: ${p => p.theme.ui.background};
-
-	border: 1px solid ${p => p.theme.ui.primaryFill};
-	border-radius: 4px;
-
-	padding: 5px 9px;
-	margin: 0 5px;
-`;
-
-const UrlSection = styled(Section)`
-	white-space: nowrap;
-	overflow: hidden;
-	text-overflow: ellipsis;
-`;
-
-const StatusSection = styled(Section)<{ $status: number }>`
-	background-color: ${p => p.theme.ui.background};
-	border-color: ${p => statusToColour(p.$status)};
-	color: ${p => statusToColour(p.$status)};
-
-	white-space: nowrap;
 `;
 
 export default ResponsePane;
---
+++ packages/app/src/features/response-pane/components/molecules/Header.tsx
@@ -0,0 +1,75 @@
+import { statusToColour } from '@beak/app/src/design-system/helpers';
+import { Flight } from '@beak/app/src/store/flight/types';
+import { constructUri } from '@beak/common/src/beak-project/url';
+import { getReasonPhrase } from 'http-status-codes';
+import React, { useState } from 'react';
+import styled from 'styled-components';
+
+export interface HeaderProps {
+	flightHistory: Flight[];
+	selectedFlightIndex: number;
+	updateSelectedFlight: (index: number) => void;
+}
+
+const Header: React.FunctionComponent<HeaderProps> = props => {
+	const { flightHistory, selectedFlightIndex, updateSelectedFlight } = props;
+	const selectedFlight = flightHistory[selectedFlightIndex];
+	const { request, response } = selectedFlight;
+
+	return (
+		<UrlHeaderWrapper>
+			<Section>
+				<strong>{request.uri.verb.toUpperCase()}</strong>
+			</Section>
+			<UrlSection>
+				{constructUri(request)}
+			</UrlSection>
+			<StatusSection $status={response.status}>
+				<strong>{response.status}</strong>
+				{' '}
+				{safeGetReasonPhrase(response.status)}
+			</StatusSection>
+		</UrlHeaderWrapper>
+	);
+};
+
+function safeGetReasonPhrase(status: number) {
+	try {
+		return getReasonPhrase(status);
+	} catch {
+		return '';
+	}
+}
+
+const UrlHeaderWrapper = styled.div`
+	display: flex;
+	margin: 25px auto;
+
+	font-size: 15px;
+`;
+
+const Section = styled.div`
+	background-color: ${p => p.theme.ui.background};
+
+	border: 1px solid ${p => p.theme.ui.primaryFill};
+	border-radius: 4px;
+
+	padding: 5px 9px;
+	margin: 0 5px;
+`;
+
+const UrlSection = styled(Section)`
+	white-space: nowrap;
+	overflow: hidden;
+	text-overflow: ellipsis;
+`;
+
+const StatusSection = styled(Section)<{ $status: number }>`
+	background-color: ${p => p.theme.ui.background};
+	border-color: ${p => statusToColour(p.$status)};
+	color: ${p => statusToColour(p.$status)};
+
+	white-space: nowrap;
+`;
+
+export default Header;
