a7f8d7ec1f5303ddb7a5d9ed014df6628f401d5a
Alexander Forbes-Reed <git@alx.red>
2020-12-21 18:40:38 +0000

    Cleanup

--- packages/electron-host/src/ipc-layer/beak-hub-manager.ts
+++ packages/electron-host/src/ipc-layer/beak-hub-manager.ts
@@ -3,18 +3,3 @@ import { ipcMain } from 'electron';
 import { listRecentProjects } from '../lib/beak-hub';
 
 ipcMain.handle('beak_hub:list_recents', async () => await listRecentProjects());
-
-ipcMain.handle('beak_hub:get_request_preference', async (_event, args) => {
-	const requestId = args as string;
-
-	// return await getRequestPreference(requestId);
-});
-
-ipcMain.handle('beak_hub:set_request_preference', async (_event, requestId, reqPrefDiff) => {
-	// const existing = await getRequestPreference(requestId);
-
-	// await setRequestPreference(requestId, {
-	// 	...existing,
-	// 	...reqPrefDiff,
-	// });
-});
