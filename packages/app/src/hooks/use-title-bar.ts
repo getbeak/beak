8f35bcf8966cc89a3c30c868e24cd7fd2f3ba258
Alexander Forbes-Reed <git@alx.red>
2020-12-21 19:20:05 +0000

    Observe title changes

--- packages/app/src/hooks/use-title-bar.ts
+++ packages/app/src/hooks/use-title-bar.ts
@@ -1,20 +1,44 @@
 import { Color, Titlebar } from 'custom-electron-titlebar';
-import { useEffect } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { useTheme } from 'styled-components';
 
 import { getGlobal } from '../globals';
 
 export default function useTitleBar() {
 	const theme = useTheme();
+	const titleBar = useRef<Titlebar>();
+	const [title, setTitle] = useState(window.document.title);
 
 	useEffect(() => {
 		if (getGlobal('platform') === 'darwin')
-			return;
+			return void 0;
 
-		// eslint-disable-next-line no-new
-		new Titlebar({
+		titleBar.current = new Titlebar({
 			icon: './images/logo.svg',
 			backgroundColor: Color.fromHex(theme.ui.surface),
 		});
+
+		const observer = new MutationObserver(mutations => {
+			const title = mutations[0].target as HTMLElement;
+
+			setTitle(title.innerText);
+		});
+
+		observer.observe(document.querySelector('title') as Node, {
+			subtree: true,
+			characterData: true,
+			childList: true,
+		});
+
+		return () => {
+			observer.disconnect();
+		};
 	}, []);
+
+	useEffect(() => {
+		if (getGlobal('platform') === 'darwin' || !titleBar.current)
+			return;
+
+		titleBar.current.updateTitle(title);
+	}, [title]);
 }
