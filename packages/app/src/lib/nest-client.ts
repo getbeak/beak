a53cae97050704e6830558dda19b06a06755abce
Alexander Forbes-Reed <git@alex.forbes.red>
2021-01-05 20:40:00 -0500

    Updated local storage client to store state

--- packages/app/src/lib/nest-client.ts
+++ packages/app/src/lib/nest-client.ts
@@ -1,5 +1,10 @@
-import { MagicState } from '@beak/common/types/nest';
+import { MagicStates } from '@beak/common/types/nest';
+import base64 from 'base64-js';
 import crpc, { Client } from 'crpc';
+
+import { LocalStorage } from './local-storage';
+
+const magicStatesKey = 'magic-states';
 
 interface CrpcOptions {
 	headers: Record<string, string>;
@@ -7,9 +12,11 @@ interface CrpcOptions {
 
 export default class NestClient {
 	private client: Client;
+	private storage: LocalStorage;
 
 	constructor(baseUrl: string) {
 		this.client = crpc(baseUrl);
+		this.storage = new LocalStorage('beak.nest-client');
 	}
 
 	async rpc<T>(path: string, body: unknown, options?: CrpcOptions) {
@@ -28,7 +35,6 @@ export default class NestClient {
 	}
 
 	async sendMagicLink(email: string) {
-		const decoder = new TextDecoder('utf8');
 		const encoder = new TextEncoder();
 		const randomState = new Uint8Array(32);
 		const randomVerifier = new Uint8Array(32);
@@ -37,23 +43,27 @@ export default class NestClient {
 		crypto.getRandomValues(randomState);
 		crypto.getRandomValues(randomVerifier);
 
-		const state = webSafeBase64(decoder.decode(randomState));
-		const codeVerifier = webSafeBase64(decoder.decode(randomVerifier));
+		const state = webSafeBase64(randomState);
+		const codeVerifier = webSafeBase64(randomVerifier);
 
 		const codeVerifierEncoded = encoder.encode(codeVerifier);
 		const codeChallengeHash = await crypto.subtle.digest('SHA-256', codeVerifierEncoded);
-		const codeChallenge = webSafeBase64(decoder.decode(codeChallengeHash));
+		const codeChallenge = webSafeBase64(new Uint8Array(codeChallengeHash));
 
-		const payload: MagicState = {
-			state,
-			codeVerifier,
-			codeChallenge,
-			redirectUri: 'https://magic.getbeak.app/callback',
-		};
+		// Create, insert, and store magic states to local storage
+		this.storage.setJsonItem(magicStatesKey, {
+			...(this.storage.getJsonItem<MagicStates>(magicStatesKey) || {}),
+			[state]: {
+				state,
+				codeVerifier,
+				codeChallenge,
+				redirectUri: 'https://magic.getbeak.app/',
+			},
+		});
 
 		await this.rpcNoAuth('2020-12-14/send_magic_link', {
 			clientId: 'client_000000C2kdCzNlbL1BqR5FeMatItU',
-			redirectUri: 'https://magic.getbeak.app/callback',
+			redirectUri: 'https://magic.getbeak.app/',
 			state,
 			codeChallengeMethod: 'S256',
 			codeChallenge,
@@ -63,30 +73,10 @@ export default class NestClient {
 	}
 }
 
-function webSafeBase64(str: string) {
-	const base64 = btoa(str);
-
+function webSafeBase64(arr: Uint8Array) {
 	return base64
+		.fromByteArray(arr)
 		.replace(/[+]/g, '-')
 		.replace(/[/]/g, '_')
 		.replace(/[=]+$/, '');
 }
-
-// async function generateAuthorizationState(env) {
-// 	const stateData = new Uint8Array(32);
-// 	const verifierData = new Uint8Array(32);
-
-// 	window.crypto.getRandomValues(stateData);
-// 	window.crypto.getRandomValues(verifierData);
-
-// 	const state = `${toUrlBase64(stateData)}|${env}`;
-// 	const verifier = toUrlBase64(verifierData);
-// 	const verifierParsedData = new window.TextEncoder('utf-8').encode(verifier);
-// 	const challengeData = await window.crypto.subtle.digest('SHA-256', verifierParsedData);
-
-// 	return {
-// 		state,
-// 		codeVerifier: verifier,
-// 		codeChallenge: toUrlBase64(new Uint8Array(challengeData)),
-// 	};
-// }
