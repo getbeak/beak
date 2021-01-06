b93e2139324f77eb5b199e0f54a1cb1ee609c29e
Alexander Forbes-Reed <git@alex.forbes.red>
2021-01-05 20:40:31 -0500

    Added sqwark!

---
+++ packages/common/src/utils/squawk.ts
@@ -0,0 +1,59 @@
+type Meta = Record<string, unknown>;
+
+export default class Squawk extends Error {
+	readonly code: string = '';
+	readonly meta: Meta | undefined = void 0;
+	readonly reasons: Squawk[] | undefined = void 0;
+
+	constructor(code: string, meta?: Meta | null, reasons?: Error[]) {
+		super(code);
+		// Error.captureStackTrace(this, this.constructor);
+
+		this.code = code;
+
+		if (meta)
+			this.meta = meta;
+
+		if (reasons) {
+			this.reasons = [];
+
+			for (const i in reasons) {
+				if (!Squawk.isSquawk(reasons[i]))
+					this.reasons.push(Squawk.coerce(reasons[i]));
+			}
+		}
+
+		defineNonSerializable(this, 'name', 'Squawk');
+		defineNonSerializable(this, 'message', this.code);
+	}
+
+	static isSquawk(error: Error) {
+		return (error instanceof Squawk);
+	}
+
+	static coerce(error: Error) {
+		if (Squawk.isSquawk(error))
+			return error as Squawk;
+
+		let newError: Squawk;
+
+		if (error instanceof Error)
+			newError = new Squawk('unknown', error.message ? { message: error.message } : void 0);
+		else
+			newError = new Squawk('unknown', error ? { error } : void 0);
+
+		if (error instanceof Error)
+			defineNonSerializable(newError, 'stack', error.stack);
+
+		return newError;
+	}
+}
+
+function defineNonSerializable(obj: any, property: string, value: any) {
+	Object.defineProperty(obj, property, {
+		value,
+		writable: false,
+		enumerable: false, // prevents json serialization
+		configurable: true, // allows redefinition
+	});
+}
