0f185a2105f320718ee495e437377e81537526b2
Alexander Forbes-Reed <git@alex.forbes.red>
2021-01-11 17:44:59 -0500

    f

--- packages/electron-host/scripts/notarize.js
+++ packages/electron-host/scripts/notarize.js
@@ -1,6 +1,4 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
-require('dotenv').config();
-
 const { notarize } = require('electron-notarize');
 
 exports.default = async function notarizing(context) {
