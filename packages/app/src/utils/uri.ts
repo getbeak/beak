e8648cd454f70f9316672ebd6c1ec5a6253a714f
Alexander Forbes-Reed <git@alex.forbes.red>
2021-01-24 09:27:53 -0500

    Moved helpers around

---
+++ packages/app/src/utils/uri.ts
@@ -0,0 +1,34 @@
+import { TypedObject } from '@beak/common/helpers/typescript';
+import { RequestOverview, VariableGroups } from '@beak/common/types/beak-project';
+
+import { parseValueParts } from '../features/variable-input/parser';
+
+interface Options {
+	includeQuery: boolean;
+	includeHash: boolean;
+}
+
+export function convertRequestToUrl(
+	selectedGroups: Record<string, string>,
+	variableGroups: VariableGroups,
+	info: RequestOverview,
+	opts?: Partial<Options>,
+) {
+	const value = parseValueParts(selectedGroups, variableGroups, info.url);
+	const url = new URL(value);
+	const options = {
+		includeQuery: true,
+		...opts,
+	};
+
+	if (options.includeQuery && info.query) {
+		for (const query of TypedObject.values(info.query).filter(q => q.enabled)) {
+			url.searchParams.set(
+				query.name,
+				parseValueParts(selectedGroups, variableGroups, query.value),
+			);
+		}
+	}
+
+	return url;
+}
