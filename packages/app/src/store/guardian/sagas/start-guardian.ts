f055ece6393976219d94997da918962c57028489
Alexander Forbes-Reed <git@alex.forbes.red>
2021-01-24 19:26:52 -0500

    Let’s not DDoS ourselves

--- packages/app/src/store/guardian/sagas/start-guardian.ts
+++ packages/app/src/store/guardian/sagas/start-guardian.ts
@@ -10,14 +10,20 @@ const intervalUpper = 2700; // 45 minutes
 export default createTakeLatestSagaSet(actions.startGuardian, function* startGuardianWorker() {
 	const client: NestClient = yield getContext('client');
 
+	let first = false;
+
 	while (true) {
+		if (!first)
+			yield delay(randomInclusiveNumber(intervalLower, intervalUpper) * 1000);
+
 		try {
 			yield call([client, client.ensureAlphaUser]);
-			yield delay(randomInclusiveNumber(intervalLower, intervalUpper));
 		} catch (error) {
 			// TODO(afr): Catch errors, sign out, change window
 			console.error(error);
 		}
+
+		first = true;
 	}
 });
 
