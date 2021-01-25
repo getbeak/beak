833235996c239f4aa4944f3d27f552ee2ed757a3
Alexander Forbes-Reed <git@alex.forbes.red>
2021-01-25 16:44:31 -0500

    Actually import encryption ipc

--- packages/electron-host/src/ipc-layer/index.ts
+++ packages/electron-host/src/ipc-layer/index.ts
@@ -1,6 +1,7 @@
 import './beak-hub-service';
 import './dialog-service';
-import './flight-service';
+import './encryption-service';
 import './explorer-service';
+import './flight-service';
 import './nest-service';
 import './project-service';
