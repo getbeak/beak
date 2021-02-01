0468b7676aad070cc13f9293f637e907aadf243e
Alexander Forbes-Reed <git@alex.forbes.red>
2021-02-01 00:38:06 +0000

    lint

--- packages/electron-host/src/ipc-layer/window-service.ts
+++ packages/electron-host/src/ipc-layer/window-service.ts
@@ -1,10 +1,11 @@
 import { IpcWindowServiceMain } from '@beak/common/ipc/window';
 import { ipcMain } from 'electron';
+
 import { closeWindow, createWelcomeWindow } from '../window-management';
 
 const service = new IpcWindowServiceMain(ipcMain);
 
-service.registerCloseSelfWindow(async (event) => {
+service.registerCloseSelfWindow(async event => {
 	closeWindow(event.sender.id);
 	createWelcomeWindow();
 });
