726b0a9e6b084930d489ce41d810edf62407c468
Alexander Forbes-Reed <git@alex.forbes.red>
2021-02-05 18:49:54 +0000

    UI improvements around actions

--- packages/app/src/features/json-editor/components/molecules/EntryActions.tsx
+++ packages/app/src/features/json-editor/components/molecules/EntryActions.tsx
@@ -1,4 +1,6 @@
 import actions from '@beak/app/store/project/actions';
+import { faMinus, faPlus } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react';
 import { useDispatch } from 'react-redux';
 import styled from 'styled-components';
@@ -17,13 +19,12 @@ const EntryActions: React.FunctionComponent<EntryActionsProps> = props => {
 		<Wrapper>
 			{!isRoot && (
 				<Button tabIndex={-1} onClick={() => {
-					// Show warning??
 					dispatch(actions.requestBodyJsonEditorRemoveEntry({
 						jPath,
 						requestId,
 					}));
 				}}>
-					{'-'}
+					<FontAwesomeIcon icon={faMinus} />
 				</Button>
 			)}
 			<Button tabIndex={-1} onClick={() => {
@@ -32,7 +33,7 @@ const EntryActions: React.FunctionComponent<EntryActionsProps> = props => {
 					requestId,
 				}));
 			}}>
-				{'+'}
+				<FontAwesomeIcon icon={faPlus} />
 			</Button>
 		</Wrapper>
 	);
@@ -54,10 +55,15 @@ const Button = styled.button`
 	padding: 0;
 
 	background: none;
-	border: 1px solid ${p => p.theme.ui.blankFill};
-	color: ${p => p.theme.ui.blankFill};
+	/* TODO(afr): Change this to a real colour, not text colour */
+	border: 1px solid ${p => p.theme.ui.textMinor};
+	color: ${p => p.theme.ui.textMinor};
 	border-radius: 100%;
 	line-height: 15px;
+
+	box-shadow: 0 0 1px 0px white inset, 0 0 1px 0px white;
+
+	font-size: 8px;
 `;
 
 export default EntryActions;
