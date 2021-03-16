98dcee6d6db7be3cfd463534e6bfd5f0bd0faf5a
Alexander Forbes-Reed <git@alex.forbes.red>
2021-03-16 23:25:47 +0000

    Added debouncer hook

---
+++ packages/app/src/hooks/use-debounce.ts
@@ -0,0 +1,15 @@
+import { useCallback, useEffect } from 'react';
+
+const useDebounce = (effect: () => void, delay: number, deps: unknown[]) => {
+	const callback = useCallback(effect, deps);
+
+	useEffect(() => {
+		const handler = setTimeout(() => {
+			callback();
+		}, delay);
+
+		return () => void clearTimeout(handler);
+	}, [callback, delay]);
+};
+
+export default useDebounce;
