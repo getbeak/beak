c03896ca391277182079738f9954bf6543ec7770
Alexander Forbes-Reed <git@alex.forbes.red>
2021-03-16 23:25:57 +0000

    Added debouncing input

---
+++ packages/app/src/components/atoms/DebouncedInput.tsx
@@ -0,0 +1,32 @@
+import useDebounce from '@beak/app/hooks/use-debounce';
+import React, { useEffect, useState } from 'react';
+
+interface DebouncedInput extends Omit<React.HTMLProps<HTMLInputElement>, 'onChange'> {
+	type: 'text';
+	innerRef?: React.Ref<HTMLInputElement> | null;
+	onChange: (value: string) => void;
+	value: string;
+}
+
+const DebouncedInput: React.FunctionComponent<DebouncedInput> = props => {
+	const { innerRef, onChange, ...rest } = props;
+	const ref = innerRef ?? null;
+	const [localValue, setLocalValue] = useState('');
+
+	useEffect(() => {
+		setLocalValue(props.value);
+	}, [props.value]);
+
+	useDebounce(() => onChange(localValue), 500, [localValue]);
+
+	return (
+		<input
+			{...rest}
+			ref={ref}
+			value={localValue}
+			onChange={e => setLocalValue(e.target.value)}
+		/>
+	);
+};
+
+export default DebouncedInput;
