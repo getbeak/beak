0209b4cd4c246384ff14dd261cfe327c55533da7
Alexander Forbes-Reed <git@alex.forbes.red>
2021-03-20 14:28:26 +0000

    Added some helper atoms

--- packages/website/src/components/atoms/Footer.tsx
+++ packages/website/src/components/atoms/Footer.tsx
@@ -1,6 +1,8 @@
 import styled from 'styled-components';
 
 const Footer = styled.footer`
+	border-top: 1px solid ${p => p.theme.ui.backgroundBorderSeparator};
+	padding-top: 20px;
 	text-align: center;
 	font-size: 14px;
 	margin-bottom: 40px;
---
+++ packages/website/src/components/atoms/ScrollTarget.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import styled from 'styled-components';
+
+interface ScrollTargetProps {
+	target: string;
+}
+
+const ScrollTarget: React.FunctionComponent<ScrollTargetProps> = ({ target }) => (
+	<Wrapper>
+		<Target id={target} />
+	</Wrapper>
+);
+
+const Wrapper = styled.div`
+	position: relative;
+`;
+
+const Target = styled.div`
+	position: absolute;
+	top: -150px;
+`;
+
+export default ScrollTarget;
