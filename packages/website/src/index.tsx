3d657c5c55ada64ee35ca0fb90eb16ade067d139
Alexander Forbes-Reed <git@alex.forbes.red>
2021-03-23 20:49:43 +0000

    removed stub

--- packages/website/src/index.tsx
+++ packages/website/src/index.tsx
@@ -5,11 +5,9 @@ import ReactDOM from 'react-dom';
 import AppContainer from './containers/App';
 import { GlobalStyle } from './design-system';
 import Home from './features/home/components/Home';
-// import { configureStore } from './store';
 
 const EntryPoint: React.FunctionComponent = () => (
 	<React.Fragment>
-		{/* <Provider store={configureStore()}> */}
 		<base href={'./'} />
 		<DesignSystemProvider themeKey={'dark'}>
 			<GlobalStyle />
@@ -17,7 +15,6 @@ const EntryPoint: React.FunctionComponent = () => (
 				<Home />
 			</AppContainer>
 		</DesignSystemProvider>
-		{/* </Provider> */}
 	</React.Fragment>
 );
 
