b4cbc3f0a7f3537f9bb13ce2a67c8a0b315f2c01
Alexander Forbes-Reed <git@alex.forbes.red>
2021-03-24 21:36:06 +0000

    Remove unused augs

--- packages/website/src/augmentations.d.ts
+++
@@ -1,7 +0,0 @@
-import { Store } from 'react-redux';
-
-// import { ApplicationState } from './store';
-
-// declare module 'react-redux' {
-// 	interface DefaultRootState extends ApplicationState {}
-// }
