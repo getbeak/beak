9d9b409200ed440d679fdccca965e6bbc3710009
Alexander Forbes-Reed <git@alex.forbes.red>
2021-03-24 22:40:30 +0000

    add state lib to persistant storage

--- packages/electron-host/src/lib/persistent-store.ts
+++ packages/electron-host/src/lib/persistent-store.ts
@@ -2,15 +2,19 @@ import { RecentLocalProject } from '@beak/common/dist/types/beak-hub';
 import { UserStore } from '@beak/common/dist/types/user';
 import ElectronStore from 'electron-store';
 
+import { WindowState } from './window-state-manager';
+
 export interface Store {
 	recents: RecentLocalProject[];
 	user: UserStore | null;
+	windowStates: Record<string, WindowState>;
 }
 
 const persistentStore = new ElectronStore<Store>({
 	defaults: {
 		recents: [],
 		user: null,
+		windowStates: {},
 	},
 });
 
