26e918f1167e925de3e0bf224df0db69d13f03ad
Alexander Forbes-Reed <git@alex.forbes.red>
2021-04-28 21:16:35 +0100

    Add auth and arbiter to persist store

--- packages/common/src/types/nest.ts
+++ packages/common/src/types/nest.ts
@@ -1,3 +1,5 @@
+export type GrantType = 'authorization_code' | 'refresh_token' | 'access_token';
+
 export interface MagicStates {
 	[state: string]: MagicState;
 }
@@ -7,4 +9,45 @@ export interface MagicState {
 	codeVerifier: string;
 	codeChallenge: string;
 	redirectUri: string;
+}
+
+export interface Grant {
+	type: GrantType;
+	value: string;
+}
+
+export interface SendMagicLinkRequest {
+	clientId: string;
+	redirectUri: string;
+	state: string;
+	codeChallengeMethod: string;
+	codeChallenge: string;
+	identifierType: string;
+	identifierValue: string;
+}
+
+export interface AuthenticateUserRequest {
+	clientId: string;
+	grantType: GrantType;
+	redirectUri?: string;
+	code: string;
+	codeVerifier?: string;
+}
+
+export interface AuthenticateUserResponse {
+	accessToken: string;
+	tokenType: string;
+	expiresIn: number;
+	expiresAt: string;
+	refreshToken: string;
+	userId: string;
+	clientId: string;
+}
+
+export interface GetSubscriptionStatusRequest {
+	userId: string;
+}
+
+export interface GetSubscriptionStatusResponse {
+	subscription: string;
 }
--- packages/electron-host/src/lib/persistent-store.ts
+++ packages/electron-host/src/lib/persistent-store.ts
@@ -1,20 +1,30 @@
 import { RecentLocalProject } from '@beak/common/dist/types/beak-hub';
-import { UserStore } from '@beak/common/dist/types/user';
+import { ArbiterStatus } from '@beak/common/types/arbiter';
+import { AuthenticateUserResponse, MagicStates } from '@beak/common/types/nest';
 import ElectronStore from 'electron-store';
 
 import { WindowState } from './window-state-manager';
 
 export interface Store {
 	recents: RecentLocalProject[];
-	user: UserStore | null;
 	windowStates: Record<string, WindowState>;
+
+	arbiter: ArbiterStatus;
+	auth: AuthenticateUserResponse | null;
+	magicStates: MagicStates;
 }
 
 const persistentStore = new ElectronStore<Store>({
 	defaults: {
 		recents: [],
-		user: null,
 		windowStates: {},
+
+		arbiter: {
+			lastCheck: '1989-12-13T00:00:00.01Z',
+			status: false,
+		},
+		auth: null,
+		magicStates: {},
 	},
 });
 
