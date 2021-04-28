16ee592e1cd69e3be441042aaaeea46e0b08ff48
Alexander Forbes-Reed <git@alex.forbes.red>
2021-04-28 21:15:03 +0100

    Remove nest store

--- packages/app/src/store/nest/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as actions } from './actions';
-export { default as reducers } from './reducers';
-export { default as sagas } from './sagas';
-export { default as types } from './types';
--- packages/app/src/store/nest/reducers.ts
+++
@@ -1,9 +0,0 @@
-import { createAsyncReducer } from '@beak/app/utils/redux/async-reducers';
-import { combineReducers } from '@reduxjs/toolkit';
-
-import { ActionTypes, initialState } from './types';
-
-export default combineReducers({
-	handleMagicLink: createAsyncReducer(ActionTypes.HANDLE_MAGIC_LINK, initialState.handleMagicLink),
-	sendMagicLink: createAsyncReducer(ActionTypes.SEND_MAGIC_LINK, initialState.sendMagicLink),
-});
