6d29e5559829b28806cda18ffd50ef27a9471c76
Alexander Forbes-Reed <git@alex.forbes.red>
2021-04-28 23:29:09 +0100

    Repurpose guardian to listen for arbiter broadcasts

--- packages/app/src/store/guardian/sagas/start-guardian.ts
+++ packages/app/src/store/guardian/sagas/start-guardian.ts
@@ -1,12 +1,32 @@
 import { createTakeLatestSagaSet } from '@beak/app/utils/redux/sagas';
-import { call, delay, getContext } from 'redux-saga/effects';
+import { eventChannel } from 'redux-saga';
+import { call, delay, getContext, take } from 'redux-saga/effects';
 
 import { actions } from '..';
+
+const { ipcRenderer } = window.require('electron');
 
 const intervalLower = 900; // 15 minutes
 const intervalUpper = 2700; // 45 minutes
 
 export default createTakeLatestSagaSet(actions.startGuardian, function* startGuardianWorker() {
+	const channel = eventChannel(emitter => {
+		ipcRenderer.on('arbiter_broadcast', (_event, args) => {
+			emitter(args);
+		});
+
+		return () => { /* */ };
+	});
+
+	while (true) {
+		const result: Event = yield take(channel);
+
+		if (result === null)
+			break;
+
+		console.log(result);
+	}
+
 	// TODO(afr): Listen to changes on the arbiter channel
 });
 
