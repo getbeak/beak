418f30aa86d4c360263d1d9eda325d308177a110
Alexander Forbes-Reed <git@alex.forbes.red>
2021-06-26 21:49:13 -0400

    Write in notification logic on flights

--- packages/app/src/store/flight/sagas/begin-flight.ts
+++ packages/app/src/store/flight/sagas/begin-flight.ts
@@ -1,21 +1,26 @@
 import binaryStore from '@beak/app/lib/binary-store';
-import { ipcFlightService } from '@beak/app/lib/ipc';
+import { ipcFlightService, ipcNotificationService } from '@beak/app/lib/ipc';
+import { getStatusReasonPhrase } from '@beak/app/utils/http';
 import {
+	RequestNode,
 	RequestOverview,
 	ResponseOverview,
 } from '@beak/common/dist/types/beak-project';
 import { TypedObject } from '@beak/common/helpers/typescript';
 import { FlightMessages } from '@beak/common/ipc/flight';
+import { statusToColour } from '@beak/design-system/helpers';
 import ksuid from '@cuvva/ksuid';
 import { PayloadAction } from '@reduxjs/toolkit';
 import { END, eventChannel } from 'redux-saga';
-import { put, take } from 'redux-saga/effects';
+import { put, select, take } from 'redux-saga/effects';
 
+import { ApplicationState } from '../..';
 import * as actions from '../actions';
 import { BeginFlightPayload } from '../types';
 
 export default function* requestFlightWorker({ payload }: PayloadAction<BeginFlightPayload>) {
 	const { binaryStoreKey, flightId, request, requestId, redirectDepth } = payload;
+	const requestNode: RequestNode = yield select((s: ApplicationState) => s.global.project.tree[requestId]);
 
 	binaryStore.create(binaryStoreKey);
 
@@ -30,6 +35,7 @@ export default function* requestFlightWorker({ payload }: PayloadAction<BeginFli
 	}
 
 	let response: ResponseOverview | null = null;
+	let error: Error | null = null;
 
 	const channel = eventChannel(emitter => {
 		ipcFlightService.registerFlightHeartbeat(async (_event, payload) => {
@@ -47,6 +53,8 @@ export default function* requestFlightWorker({ payload }: PayloadAction<BeginFli
 		});
 
 		ipcFlightService.registerFlightFailed(async (_event, payload) => {
+			error = payload.error;
+
 			emitter(actions.flightFailure({ flightId, requestId, error: payload.error }));
 			emitter(END);
 		});
@@ -71,8 +79,32 @@ export default function* requestFlightWorker({ payload }: PayloadAction<BeginFli
 			yield put(result);
 		}
 	} finally {
-		if (response)
-			yield detectAndHandleRedirect(request, response, redirectDepth, requestId);
+		switch (true) {
+			case error: {
+				ipcNotificationService.sendNotification({
+					title: 'Request failed',
+					body: `${requestNode.name} failed in transit`,
+				});
+
+				break;
+			}
+
+			case response: {
+				const redirected: boolean = yield detectAndHandleRedirect(request, response!, redirectDepth, requestId);
+
+				if (redirected)
+					break;
+
+				ipcNotificationService.sendNotification({
+					title: `${requestNode.name} - ${response!.status} ${getStatusReasonPhrase(response!.status)}`,
+					body: request.url[0] as string,
+				});
+
+				break;
+			}
+
+			default: break;
+		}
 	}
 }
 
@@ -80,14 +112,14 @@ function* detectAndHandleRedirect(request: RequestOverview, response: ResponseOv
 	const withinRedirectRange = response.status >= 300 && response.status < 400;
 
 	if (!withinRedirectRange || !request.options.followRedirects)
-		return;
+		return false;
 
 	const url = new URL(request.url[0] as string); // hack
 	const locationHeader = TypedObject.keys(response.headers).find(h => h.toLowerCase() === 'location');
 	const location = response.headers[locationHeader ?? ''];
 
 	if (!location)
-		return;
+		return false;
 
 	const binaryStoreKey = ksuid.generate('binstore').toString();
 	const flightId = ksuid.generate('flight').toString();
@@ -109,4 +141,6 @@ function* detectAndHandleRedirect(request: RequestOverview, response: ResponseOv
 		request: newRequest,
 		requestId: reqId,
 	}));
+
+	return true;
 }
