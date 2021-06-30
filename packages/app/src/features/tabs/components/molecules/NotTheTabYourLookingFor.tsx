e6a851207e69da6a972779e87df10d56d5d0ec15
Alexander Forbes-Reed <git@alex.forbes.red>
2021-06-30 15:21:48 -0400

    Improve no tab page

--- packages/app/src/features/tabs/components/molecules/NotTheTabYourLookingFor.tsx
+++ packages/app/src/features/tabs/components/molecules/NotTheTabYourLookingFor.tsx
@@ -1,12 +1,46 @@
+import Button from '@beak/app/components/atoms/Button';
+import { tabSelected } from '@beak/app/store/project/actions';
+import { TypedObject } from '@beak/common/helpers/typescript';
+import { RequestNode } from '@beak/common/types/beak-project';
 import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import styled from 'styled-components';
 
-const NotTheTabYourLookingFor: React.FunctionComponent = () => (
-	<Wrapper>
-		<Header>{'This is not the tab you\'re looking for...'}</Header>
-		<Body>{'Also not really sure how you did this 🤔'}</Body>
-	</Wrapper>
-);
+const NotTheTabYourLookingFor: React.FunctionComponent = () => {
+	const tree = useSelector(s => s.global.project.tree);
+	const dispatch = useDispatch();
+
+	function spinThatWheel() {
+		const requests = TypedObject.values(tree).filter(n => n.type === 'request') as RequestNode[];
+
+		if (requests.length === 0) {
+			dispatch(tabSelected({
+				type: 'renderer',
+				payload: 'variable_group_editor',
+				temporary: false,
+			}));
+
+			return;
+		}
+
+		dispatch(tabSelected({
+			type: 'request',
+			payload: requests[Math.floor(Math.random() * requests.length)].id,
+			temporary: false,
+		}));
+	}
+
+	return (
+		<Wrapper>
+			<Header>{'This is not the tab you\'re looking for'}</Header>
+			<Body>{'Why not select a request to get going, or...'}</Body>
+
+			<Button onClick={() => spinThatWheel()}>
+				{'Spin the wheel!'}
+			</Button>
+		</Wrapper>
+	);
+}
 
 const Wrapper = styled.div`
 	height: 100%;
@@ -19,7 +53,8 @@ const Header = styled.h1`
 	margin: 0;
 	font-weight: 400;
 	font-size: 35px;
-	color: ${p => p.theme.ui.textOnFill};
+	line-height: 25px;
+	color: ${p => p.theme.ui.textOnSurfaceBackground};
 `;
 
 const Body = styled.p`
