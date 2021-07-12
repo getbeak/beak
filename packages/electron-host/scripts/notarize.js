4b1eabd3d0cadd79a13144716afd76714f58a7f4
Alexander Forbes-Reed <git@alex.forbes.red>
2021-07-12 22:23:06 +0100

    Fixed notarizing

--- packages/electron-host/scripts/notarize.js
+++ packages/electron-host/scripts/notarize.js
@@ -1,5 +1,7 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 const { notarize } = require('electron-notarize');
+const AdmZip = require('adm-zip');
+const path = require('path');
 
 exports.default = async function notarizing(context) {
 	const { electronPlatformName, appOutDir } = context;
@@ -8,12 +10,19 @@ exports.default = async function notarizing(context) {
 		return void 0;
 
 	const appName = context.packager.appInfo.productFilename;
+	const appFilePath = path.join(appOutDir, `${appName}.app`);
 
-	return await notarize({
+	await notarize({
 		appBundleId: 'app.getbeak.beak',
-		appPath: `${appOutDir}/${appName}.app`,
+		appPath: appFilePath,
 		appleId: process.env.APPLE_ID,
 		appleIdPassword: process.env.APPLE_ID_PASSWORD,
 		ascProvider: process.env.ASC_PROVIDER,
 	});
+
+	const archive = new AdmZip();
+	const zipFilePath = path.join(appOutDir, `${appName}.zip`);
+
+	archive.addLocalFolder(appFilePath);
+	archive.writeZip(zipFilePath);
 };
