e64a798563c87b7b0a1d6b4dfbcd3f791622f379
Alexander Forbes-Reed <git@alex.forbes.red>
2021-08-22 19:01:12 +0100

    tweaks

--- packages/website/webpack.config.js
+++ packages/website/webpack.config.js
@@ -21,7 +21,7 @@ module.exports = {
 	output: {
 		path: path.join(__dirname, 'dist'),
 		publicPath: '/',
-		filename: '[name].[contenthash].js',
+		filename: '[name].[hash].js',
 	},
 	module: {
 		rules: [{
