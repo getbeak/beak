b1d3bd42d47190cd036eca53e1b21dcbbd6130a8
Alexander Forbes-Reed <git@alex.forbes.red>
2021-08-22 19:01:20 +0100

    Added router to react app

--- packages/website/src/index.tsx
+++ packages/website/src/index.tsx
@@ -1,21 +1,44 @@
 import { DesignSystemProvider } from '@beak/design-system';
+import { ConnectedRouter } from 'connected-react-router';
+import { createBrowserHistory } from 'history';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { Route, Switch } from 'react-router';
 
 import AppContainer from './containers/App';
 import { GlobalStyle } from './design-system';
 import Home from './features/home/components/Home';
+import { configureStore } from './store';
+
+const history = createBrowserHistory();
+const store = configureStore(history);
 
 const EntryPoint: React.FunctionComponent = () => (
-	<React.Fragment>
-		<base href={'./'} />
-		<DesignSystemProvider themeKey={'dark'}>
-			<GlobalStyle />
-			<AppContainer>
-				<Home />
-			</AppContainer>
-		</DesignSystemProvider>
-	</React.Fragment>
+	<Provider store={store}>
+		<ConnectedRouter history={history}>
+			<base href={'./'} />
+			<DesignSystemProvider themeKey={'dark'}>
+				<GlobalStyle />
+				<AppContainer>
+					<Switch>
+						<Route exact path={'/'}>
+							<Home />
+						</Route>
+						<Route exact path={'/pricing'}>
+							{'todo'}
+						</Route>
+						<Route exact path={'/purchase/complete'}>
+							{'todo'}
+						</Route>
+						<Route>
+							{'404'}
+						</Route>
+					</Switch>
+				</AppContainer>
+			</DesignSystemProvider>
+		</ConnectedRouter>
+	</Provider>
 );
 
 ReactDOM.render(<EntryPoint />, document.getElementById('root'));
--- packages/website/src/store/index.ts
+++ packages/website/src/store/index.ts
@@ -1,23 +1,27 @@
-import { applyMiddleware, combineReducers, createStore } from 'redux';
+import { connectRouter, routerMiddleware } from 'connected-react-router';
+import type { History } from 'history';
+import { applyMiddleware, combineReducers, compose, createStore } from 'redux';
 
 export interface ApplicationState { }
 
-function createRootReducer() {
-	return combineReducers<ApplicationState>({ });
+function createRootReducer(history: History) {
+	return combineReducers<ApplicationState>({
+		router: connectRouter(history),
+	});
 }
 
 function createInitialState(): ApplicationState {
 	return { };
 }
 
-// export function configureStore() {
-// 	const initialState = createInitialState();
+export function configureStore(history: History) {
+	const initialState = createInitialState();
 
-// 	const store = createStore(
-// 		createRootReducer(),
-// 		initialState,
-// 		// composeEnhancers(applyMiddleware()),
-// 	);
+	const store = createStore(
+		createRootReducer(history),
+		initialState,
+		compose(applyMiddleware(routerMiddleware(history))),
+	);
 
-// 	return store;
-// }
+	return store;
+}
