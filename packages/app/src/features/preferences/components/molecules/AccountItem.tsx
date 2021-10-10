1ce657023765e0ee5da5e2c7f338a14d5bbbd8f1
Alexander Forbes-Reed <git@alex.forbes.red>
2021-10-10 17:33:12 +0100

    Finished account block

--- packages/app/src/features/preferences/components/molecules/AccountItem.tsx
+++ packages/app/src/features/preferences/components/molecules/AccountItem.tsx
@@ -1,21 +1,44 @@
+import { ipcNestService } from '@beak/app/lib/ipc';
+import { GetUserResponse } from '@beak/common/types/nest';
 import { toHexAlpha } from '@beak/design-system/utils';
-import { faDove } from '@fortawesome/free-solid-svg-icons';
+import { faUserCircle } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import styled, { useTheme } from 'styled-components';
 
 const AccountItem: React.FunctionComponent = () => {
 	const theme = useTheme();
+	const [primaryEmail, setPrimaryEmail] = useState<string | null>(null);
+
+	useEffect(() => {
+		ipcNestService.getUser().then(user => {
+			const identifiers = user.identifiers
+				.filter(i => i.identifierType === 'email' && i.removedAt === null)
+				.sort((a, b) => a.createdAt.localeCompare(b.createdAt));
+
+			const verifiedEmail = identifiers.find(i => i.verifiedAt !== null);
+			const backup = identifiers[0];
+
+			if (!backup)
+				return;
+
+			setPrimaryEmail((verifiedEmail ?? backup).identifierValue);
+		});
+	}, []);
+
+	if (!primaryEmail)
+		return null;
 
 	return (
 		<Wrapper>
 			<FontAwesomeIcon
-				icon={faDove}
+				icon={faUserCircle}
 				size={'1x'}
 				color={theme.ui.primaryFill}
 			/>
-			<br />
-			{'taylor.swift@getbeak.app'}
+			<AccountEmail title={primaryEmail}>
+				{primaryEmail}
+			</AccountEmail>
 		</Wrapper>
 	);
 };
@@ -26,6 +49,14 @@ const Wrapper = styled.div`
 	padding: 10px;
 	background: ${p => toHexAlpha(p.theme.ui.surface, 0.25)};
 	color: ${p => p.theme.ui.textMinor};
+`;
+
+const AccountEmail = styled.abbr`
+	display: block;
+	overflow: hidden;
+	word-wrap: break-word;
+	white-space: nowrap;
+	text-overflow: ellipsis;
 `;
 
 export default AccountItem;
