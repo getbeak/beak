a7918623f4d635a4c28f14b6eb87f1c631f53b57
Alexander Forbes-Reed <git@alex.forbes.red>
2022-04-08 18:51:03 +0100

    Improve variable group project sidebar UX

---
+++ packages/app/src/components/atoms/Abbr.ts
@@ -0,0 +1,5 @@
+import styled from 'styled-components';
+
+export const SilentAbbr = styled.abbr`
+	text-decoration: none;
+`;
--- packages/app/src/features/project-pane/components/organisms/VariableGroups.tsx
+++ packages/app/src/features/project-pane/components/organisms/VariableGroups.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import { SilentAbbr } from '@beak/app/components/atoms/Abbr';
 import useSectionBody from '@beak/app/features/sidebar/hooks/use-section-body';
 import { editorPreferencesSetSelectedVariableGroup } from '@beak/app/store/preferences/actions';
 import { TypedObject } from '@beak/common/helpers/typescript';
@@ -26,23 +27,21 @@ const VariableGroups: React.FunctionComponent = () => {
 				return (
 					<Item key={k}>
 						<GroupName>
-							{k}
+							<SilentAbbr title={k}>{k}</SilentAbbr>
 						</GroupName>
 
-						<Selector>
-							<select
-								value={value}
-								onChange={e => {
-									dispatch(editorPreferencesSetSelectedVariableGroup({
-										variableGroup: k,
-										groupId: e.target.value,
-									}));
-								}}
-							>
-								{groupKeys.map(gk => (
-									<option key={gk} value={gk}>{groups[gk]}</option>
-								))}
-							</select>
+						<Selector
+							value={value}
+							onChange={e => {
+								dispatch(editorPreferencesSetSelectedVariableGroup({
+									variableGroup: k,
+									groupId: e.target.value,
+								}));
+							}}
+						>
+							{groupKeys.map(gk => (
+								<option key={gk} value={gk}>{groups[gk]}</option>
+							))}
 						</Selector>
 					</Item>
 				);
@@ -53,46 +52,50 @@ const VariableGroups: React.FunctionComponent = () => {
 
 const Container = styled.div`
 	padding: 4px 5px;
-	padding-right: 3px;
+	padding-right: 0;
 	padding-bottom: 0;
-
-	max-height: 120px;
-	min-height: 26px;
 `;
 
 const Item = styled.div`
-	display: flex;
+	display: grid;
+	grid-template-columns: 1fr 1fr;
 	justify-content: space-between;
 	align-items: center;
+	gap: 10px;
 	margin: 4px 0;
+	max-width: 100%;
 
 	&:first-child {
 		margin-top: 0;
 	}
+
+	&:not(:last-child) {
+		margin-bottom: 6px;
+	}
 `;
 
 const GroupName = styled.span`
-	min-width: 80px;
 	color: ${p => p.theme.ui.textMinor};
 	font-size: 12px;
+
+	overflow: hidden;
+	text-overflow: ellipsis;
+	white-space: nowrap;
 `;
 
-const Selector = styled.div`
-	position: relative;
-	margin-right: -3px;
+const Selector = styled.select`
+	width: 100%;
+	font-size: 12px;
+	border: 0;
+	border-radius: 4px;
+	background: none;
+	color: ${p => p.theme.ui.textMinor};
+	text-align-last: right;
+	text-overflow: ellipsis;
 
-	> select {
-		font-size: 12px;
-		border: 0;
-		border-radius: 4px;
-		background: none;
-		color: ${p => p.theme.ui.textMinor};
-		text-align-last: right;
-
-		&:hover, &:active, &:focus {
-			background: ${p => p.theme.ui.surface};
-			outline: none;
-		}
+	&:hover, &:active, &:focus {
+		background: ${p => p.theme.ui.surface};
+		outline: none;
 	}
 `;
 
