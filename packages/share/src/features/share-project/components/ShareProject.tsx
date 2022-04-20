086548ab9008b042865ffe9ffde0dcc3bc397997
Alexander Forbes-Reed <git@alex.forbes.red>
2022-04-20 18:06:34 +0100

    [web-prod] New social sharing

--- packages/share/src/features/share-project/components/ShareProject.tsx
+++ packages/share/src/features/share-project/components/ShareProject.tsx
@@ -8,24 +8,7 @@ import InfoCard from './molecules/InfoCard';
 const ShareProject: React.FunctionComponent = () => (
 	<SpacedContainer>
 		<Helmet defer={false}>
-			{/* <!-- Primary Meta Tags --> */}
 			<title>{'Beak :: Someone has shared a project with you'}</title>
-			<meta name={'title'} content={'Someone has shared a project with you'} />
-			<meta name={'description'} content={'Stylish and clean UI, powerful git-based revision control'} />
-
-			{/* <!-- Open Graph / Facebook --> */}
-			<meta property={'og:type'} content={'website'} />
-			<meta property={'og:url'} content={'https://share.getbeak.app/'} />
-			<meta property={'og:title'} content={'Someone has shared a project with you'} />
-			<meta property={'og:description'} content={'Stylish and clean UI, powerful git-based revision control'} />
-			{/* <meta property={'og:image'} content={'/assets/home.png'} /> */}
-
-			{/* <!-- Twitter --> */}
-			<meta property={'twitter:card'} content={'summary_large_image'} />
-			<meta property={'twitter:url'} content={'https://share.getbeak.app/'} />
-			<meta property={'twitter:title'} content={'Someone has shared a project with you'} />
-			<meta property={'twitter:description'} content={'Stylish and clean UI, powerful git-based revision control'} />
-			{/* <meta property={'twitter:image'} content={'/assets/home.png'} /> */}
 		</Helmet>
 		<InfoCard />
 	</SpacedContainer>
