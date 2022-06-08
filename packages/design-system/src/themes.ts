11ed88328ad2779f688f478fe6a6a365c9e5b0da
Alexander Forbes-Reed <git@alex.forbes.red>
2022-06-08 15:49:48 +0100

    Started on light theme colours

--- packages/design-system/src/themes.ts
+++ packages/design-system/src/themes.ts
@@ -41,7 +41,7 @@ const lightTheme: UIColors = {
 	primaryFill: '#d45d80',
 	secondaryFill: '#333399',
 	tertiaryFill: '#33CC99',
-	surfaceFill: '#1A1E2D',
+	surfaceFill: '#ffffff',
 	blankFill: '#fff',
 	alertFill: '#FC3233',
 
@@ -52,26 +52,26 @@ const lightTheme: UIColors = {
 	destructiveAction: '#FC3233',
 	destructiveActionMuted: '#DB182C',
 
-	background: '#F7F7FF',
+	background: '#ffffff',
 	secondaryBackground: '#fff',
 	blankBackground: '#fff',
 
-	surface: '#fff',
-	surfaceHighlight: '#1b1e2b',
-	secondarySurface: '#3A3F55',
+	surface: '#f3f3f3',
+	surfaceHighlight: '#ececec',
+	secondarySurface: '#ececec',
 
-	backgroundBorderSeparator: '#4a4651',
-	surfaceBorderSeparator: '#1A1E2D',
+	backgroundBorderSeparator: '#dadada',
+	surfaceBorderSeparator: '#d7d7d7',
 
-	textOnAction: '#fff',
-	textOnFill: '#a6accd',
+	textOnAction: '#2e2e2e',
+	textOnFill: '#616161',
 	textHighlight: '#d45d80',
 	textSuccess: '#33CC99',
 	textAlert: '#FC3233',
-	textOnSurfaceBackground: '#fff',
+	textOnSurfaceBackground: '#2e2e2e',
 	textOnSurfaceBackgroundMuted: '#4B5178',
-	textMinor: '#BEBEC6',
-	textMinorMuted: '#a6accd',
+	textMinor: '#616161',
+	textMinorMuted: '#989899',
 };
 
 function createUiColors(theme: Theme): UIColors {
