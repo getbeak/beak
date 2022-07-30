e9e327e2687d32a795068e57a633ff29915fdd11
Alexander Forbes-Reed <git@alex.forbes.red>
2022-07-30 11:37:39 +0200

    Final fixes for offline monaco

--- packages/app/src/utils/monaco.ts
+++ packages/app/src/utils/monaco.ts
@@ -1,8 +1,32 @@
 import { EditorProps, loader } from '@monaco-editor/react';
 import * as monaco from 'monaco-editor';
+import editorWorker from 'monaco-editor/esm/vs/editor/editor.worker?worker';
+import cssWorker from 'monaco-editor/esm/vs/language/css/css.worker?worker';
+import htmlWorker from 'monaco-editor/esm/vs/language/html/html.worker?worker';
+import jsonWorker from 'monaco-editor/esm/vs/language/json/json.worker?worker';
+import tsWorker from 'monaco-editor/esm/vs/language/typescript/ts.worker?worker';
+
+// @ts-expect-error
+self.MonacoEnvironment = {
+	getWorker(_: unknown, label: string) {
+		if (label === 'json')
+			return new jsonWorker();
+
+		if (label === 'css' || label === 'scss' || label === 'less')
+			return new cssWorker();
+
+		if (label === 'html' || label === 'handlebars' || label === 'razor')
+			return new htmlWorker();
+
+		if (label === 'typescript' || label === 'javascript')
+			return new tsWorker();
+
+		return new editorWorker();
+	},
+};
 
 // Modified version of code from https://github.com/microsoft/monaco-editor/issues/2209
-const httpLanguageDefinition = {
+const httpLanguageDefinition: monaco.languages.IMonarchLanguage = {
 	ignoreCase: true,
 	includeLF: true,
 	defaultToken: 'source',
@@ -13,9 +37,11 @@ const httpLanguageDefinition = {
 			[/^(?:[A-Z0-9]+\s)/, 'comment', '@overview'],
 		],
 		overview: [
+			// @ts-expect-error
 			[/(.+\s)(.*)(\n)$/, ['variable.name', 'source', { token: 'source', next: '@headers' }]],
 		],
 		headers: [
+			// @ts-expect-error
 			[/^(content-type\s*)(:)/, ['variable.name', { token: 'delimiter', next: '@content_type' }]],
 			[/^([^:]+)(:)(.+\n)$/, ['variable.name', 'delimiter', 'source']],
 
@@ -33,6 +59,7 @@ const httpLanguageDefinition = {
 
 		content_type: [
 			// Pull out mime-type so we can style the body accordingly
+			// @ts-expect-error
 			[/(\s*)([a-z0-9.-]+\/[a-z0-9.+-]+)(.*\n)$/, ['source', { token: 'source', next: '@headers.$2' }, 'source']],
 
 			[/./, '@rematch', '@pop'],
