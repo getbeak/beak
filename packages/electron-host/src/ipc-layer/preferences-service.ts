5db36b6d11befbfaa194e5cc7efe38d5e97951c1
Alexander Forbes-Reed <git@alex.forbes.red>
2022-08-17 20:19:19 +0200

    Added broadcast to refresh UI on change

--- packages/electron-host/src/ipc-layer/preferences-service.ts
+++ packages/electron-host/src/ipc-layer/preferences-service.ts
@@ -1,3 +1,4 @@
+import { TypedObject } from '@beak/common/helpers/typescript';
 import { IpcPreferencesServiceMain } from '@beak/common/ipc/preferences';
 import { app, ipcMain } from 'electron';
 
@@ -5,6 +6,7 @@ import nestClient from '../lib/nest-client';
 import persistentStore, { Environment } from '../lib/persistent-store';
 import { setThemeMode } from '../lib/theme-manager';
 import { switchEnvironment } from '../utils/environment';
+import { windowStack } from '../window-management';
 
 const service = new IpcPreferencesServiceMain(ipcMain);
 
@@ -16,7 +18,16 @@ service.registerSetNotificationValue(async (_event, { key, value }) => persisten
 
 service.registerGetEditorOverview(async () => persistentStore.get('editor'));
 service.registerGetEditorValue(async (_event, key) => persistentStore.get(`editor.${key}`));
-service.registerSetEditorValue(async (_event, { key, value }) => persistentStore.set(`editor.${key}`, value));
+service.registerSetEditorValue(async (_event, { key, value }) => {
+	persistentStore.set(`editor.${key}`, value);
+
+	TypedObject.values(windowStack).forEach(window => {
+		if (!window)
+			return;
+
+		window.webContents.send('editor_preferences_updated');
+	});
+});
 
 service.registerGetThemeMode(async () => persistentStore.get('themeMode'));
 
