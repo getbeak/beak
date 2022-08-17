7234e65524508c614754536af843a8d4b2d861f9
Alexander Forbes-Reed <git@alex.forbes.red>
2022-08-17 20:19:27 +0200

    Moved code around

--- packages/app/src/features/preferences/components/atoms/item.ts
+++ packages/app/src/features/preferences/components/atoms/item.ts
@@ -12,11 +12,27 @@ export const ItemLabel = styled.div`
 `;
 
 export const ItemInfo = styled.div`
-	font-size: 14px;
+	font-size: 12px;
 	color: ${p => p.theme.ui.textMinorMuted};
-	margin-bottom: 5px;
+	margin: 5px 0;
 `;
 
 export const ItemSpacer = styled.div`
 	height: 5px;
+`;
+
+export const SubItemGroup = styled.div`
+	display: flex;
+	flex-direction: column;
+	gap: 8px;
+	font-size: 14px;
+`;
+
+export const SubItem = styled.div`
+	font-size: 12px;
+	color: ${p => p.theme.ui.textMinor};
+`;
+
+export const SubItemLabel = styled.div`
+	margin-bottom: 3px;
 `;
