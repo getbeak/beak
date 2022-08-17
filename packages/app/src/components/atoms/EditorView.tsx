90c238419496c2eff96228788111ba8acbc81520
Alexander Forbes-Reed <git@alex.forbes.red>
2022-08-17 20:19:49 +0200

    Added preference getting to editor

--- packages/app/src/components/atoms/EditorView.tsx
+++ packages/app/src/components/atoms/EditorView.tsx
@@ -1,5 +1,9 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import useForceReRender from '@beak/app/hooks/use-force-rerender';
+import { ipcPreferencesService } from '@beak/app/lib/ipc';
 import { createDefaultOptions } from '@beak/app/utils/monaco';
+import { EditorPreferences } from '@beak/common/types/preferences';
+import { Theme, ThemeMode } from '@beak/common/types/theme';
 import Editor, { EditorProps } from '@monaco-editor/react';
 import { useTheme } from 'styled-components';
 
@@ -7,17 +11,42 @@ interface EditorViewProps extends EditorProps { }
 
 const EditorView: React.FC<EditorViewProps> = props => {
 	const { theme } = useTheme();
+	const [preferences, setPreferences] = useState<EditorPreferences>();
+	const [latestRender, forceRerender] = useForceReRender();
+
+	useEffect(() => void ipcPreferencesService.getEditorOverview().then(setPreferences), [latestRender]);
+	useEffect(() => {
+		function listener() {
+			forceRerender();
+		}
+
+		window.secureBridge.ipc.on('editor_preferences_updated', listener);
+
+		return () => {
+			window.secureBridge.ipc.off('editor_preferences_updated', listener);
+		};
+	}, []);
+
+	if (!preferences)
+		return null;
 
 	return (
 		<Editor
-			theme={theme === 'dark' ? 'vs-dark' : 'light'}
+			theme={getRenderedTheme(theme, preferences.themeOverride)}
 			{...props}
 			options={{
-				...createDefaultOptions(),
+				...createDefaultOptions(preferences),
 				...props.options,
 			}}
 		/>
 	);
 };
+
+function getRenderedTheme(theme: Theme, preferenceThemeMode: ThemeMode) {
+	if (preferenceThemeMode === 'system')
+		return theme === 'dark' ? 'vs-dark' : 'light';
+
+	return preferenceThemeMode === 'dark' ? 'vs-dark' : 'light';
+}
 
 export default EditorView;
