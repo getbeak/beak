5e6800c6878218668edafeb08c322c3126705044
Alexander Forbes-Reed <git@alex.forbes.red>
2022-09-08 22:53:27 +0200

    Moved arbiter logic to hook

---
+++ packages/app/src/features/arbiter/hooks/use-arbiter-locking.ts
@@ -0,0 +1,33 @@
+import { useAppSelector } from '@beak/app/store/redux';
+import { differenceInDays } from 'date-fns';
+
+export default function useArbiterLocking() {
+	const arbiter = useAppSelector(s => s.global.arbiter.status);
+	const now = new Date();
+	const lastSuccessfulCheck = new Date(arbiter.lastSuccessfulCheck);
+	const sinceLastCheck = differenceInDays(now, lastSuccessfulCheck);
+	const showWarning = sinceLastCheck > 1;
+
+	const friendlyLockNotice = (() => {
+		const daysUntilLock = 5 - sinceLastCheck;
+
+		switch (true) {
+			case daysUntilLock > 2:
+				return `in ${daysUntilLock} days`;
+
+			case daysUntilLock === 1:
+				return 'tomorrow';
+
+			default:
+			case daysUntilLock <= 0:
+				return 'today';
+		}
+	})();
+
+	return {
+		lastSuccessfulCheck,
+		sinceLastCheck,
+		showWarning: true,
+		friendlyLockNotice,
+	};
+}
