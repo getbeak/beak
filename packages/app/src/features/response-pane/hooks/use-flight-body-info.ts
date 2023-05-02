a3d642cd333f956145b139ce36a1da6b72370ff0
Alexander Forbes-Reed <git@alex.forbes.red>
2023-05-02 18:20:55 +0200

    Fix types

--- packages/app/src/features/response-pane/hooks/use-flight-body-info.ts
+++ packages/app/src/features/response-pane/hooks/use-flight-body-info.ts
@@ -2,6 +2,7 @@ import { useMemo } from 'react';
 import binaryStore from '@beak/app/lib/binary-store';
 import { requestAllowsBody } from '@beak/app/utils/http';
 import type { Flight } from '@getbeak/types/flight';
+import { RequestBodyFile } from 'packages/types/request';
 
 export type NotEligible = 'request_invalid_body' | 'request_no_body' | 'response_no_body';
 type ReturnType = ['eligible', Uint8Array] | [NotEligible, null];
@@ -25,7 +26,10 @@ export default function useFlightBodyInfo(flight: Flight, mode: 'request' | 'res
 			if (request.body.type === 'text')
 				return ['eligible', encoder.encode(request.body.payload)];
 
-			return ['eligible', request.body.payload.__hacky__binaryFileData! as Uint8Array];
+			return [
+				'eligible',
+				(request.body as RequestBodyFile).payload.__hacky__binaryFileData! as Uint8Array,
+			];
 		}
 
 		if (!response || !response.hasBody)
