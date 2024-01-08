1d45538ceba5d512454bc57029001c418286991b
Alexander Forbes-Reed <git@alex.forbes.red>
2024-01-08 19:14:24 +0100

    fix: electron not finding new static path for ui dist (#577)
    Fix path not found

--- apps-host/electron/src/utils/static-path.ts
+++ apps-host/electron/src/utils/static-path.ts
@@ -1,3 +1,4 @@
+/* eslint-disable operator-linebreak */
 import * as path from 'path';
 
 // Is our app packaged in a binary or still in Electron?
@@ -10,6 +11,8 @@ export const appIsPackaged = !process.defaultApp;
  * @see https://github.com/electron-userland/electron-webpack/issues/52
  * @see https://github.com/electron-userland/electron-webpack/issues/157
  */
-const staticPath = appIsPackaged ? __dirname.replace(/app\.asar$/, 'static') : path.join(process.cwd(), 'static');
+const staticPath = appIsPackaged
+	? path.join(__dirname.replace(/app\.asar$/, 'static'), 'ui', 'dist')
+	: path.join(process.cwd(), 'static', 'ui', 'dist');
 
 export { staticPath };
